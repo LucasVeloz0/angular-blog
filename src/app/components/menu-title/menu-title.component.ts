ec556f9d6920763f201ad1b4a79d85852479a239
LucasVeloz0 <lcsxlucas@gmail.com>
2024-01-02 13:27:17 -0300

    components created with styles

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,16 @@
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-menu-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-title.component.html',
+  styleUrl: './menu-title.component.css'
+})
+export class MenuTitleComponent {
+  
+}
